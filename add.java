641d8df1f49dbf5c409c83a77103fc637d74afcd
Kushal Shah <kushalshah2512@example.com>
2025-11-25 01:00:55 +0530

    Add two numbers without using any arithmetic operators

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class add{
+    public static void main(String args[]){
+        int a=5,b=7;
+        while(b!=0){
+            int carry = a&b;
+            a=a^b;
+            b=carry << 1;
+        }
+        System.out.println("Sum: "+a);
+    }
+}
